606f3c15ae589c68a768d5fa447479a8cfeb63a2
gfcapalbo <giovanni@therp.nl>
2016-08-29 11:26:28 +0200

    cleaning HTML in compute fix headers refactoring

--- website_blog_layout_options/models/blog_post.py
+++ website_blog_layout_options/models/blog_post.py
@@ -1,23 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    This module copyright (C) 2015 Therp BV (<http://therp.nl>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openerp import models, fields, api
+from openerp import tools
 from lxml import etree
 
 
@@ -28,33 +13,36 @@ class BlogPost(models.Model):
     @api.one
     @api.onchange('extract_auto', 'display_type', 'content', 'teaser_input')
     def _extract_teaser(self):
-        if self.display_type == "teaser":
-            # no empty teasers
-            if (self.teaser_input or '') and (not self.extract_auto):
-                self.teaser = self.teaser_input
+        if self.display_type != "teaser":
+            return
+        # no empty teasers
+        if (self.teaser_input) and (not self.extract_auto):
+            self.teaser = tools.html_email_clean(self.teaser_input)
+        else:
+            res = ""
+            # limit length to roughly 3-4 lines.
+            teaser_length = 500
+            parser = etree.HTMLParser()
+            if self.content:
+                tree = etree.fromstring(self.content, parser)
+                paragraphs = tree.xpath('//p')
+                # get the first non empty paragraph
+                for paragraph in paragraphs:
+                    if paragraph.text and len(res) < teaser_length:
+                        res = res + paragraph.text + '\n'
+                    else:
+                        break
+                # trim it to the intended length
+                self.teaser = tools.html_email_clean(
+                    res[:teaser_length] + " ...")
+                self.teaser_input = tools.html_email_clean( 
+                    res[:teaser_length] + " ...")
             else:
-                res = ""
-                # limit length to roughly 3-4 lines.
-                teaser_length = 500
-                parser = etree.HTMLParser()
-                if self.content:
-                    tree = etree.fromstring(self.content, parser)
-                    paragraphs = tree.xpath('//p')
-                    # get the first non empty paragraph
-                    for paragraph in paragraphs:
-                        if paragraph.text and len(res) < teaser_length:
-                            res = res + paragraph.text + '\n'
-                        else:
-                            break
-                    # trim it to the intended length
-                    self.teaser = res[:teaser_length] + " ..."
-                    self.teaser_input = res[:teaser_length] + " ..."
-                else:
-                    # has no teaser or content,  just revert.
-                    # frontend controls needed not to have a bad workflow.
-                    # content cannot be inserted in backend by default.
-                    # add content to backend in view
-                    self.display_type = "no_teaser"
+                # has no teaser or content,  just revert.
+                # frontend controls needed not to have a bad workflow.
+                # content cannot be inserted in backend by default.
+                # add content to backend in view
+                self.display_type = "no_teaser"
 
     @api.onchange('blog_id')
     def set_new_default(self):
