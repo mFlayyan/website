a8af0e1bd93c02eea0d1295816c5299b90e17416
gfcapalbo <giovanni@therp.nl>
2016-09-05 11:40:42 +0200

    First commit adding width module

---
+++ website_blog_larger_right_column/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "website_blog_larger_right_column",
+    "version": "8.0.1.0.0",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ['website_blog'
+    ],
+    "data": [
+        'views/templates.xml',
+    ],
+    "pre_init_hook": False,
+    "post_init_hook": False,
+    "uninstall_hook": False,
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+}
